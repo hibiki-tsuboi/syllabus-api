336fe0ce206c209df094a772ad52060ac47d0837
hibiki_tsuboi <hibiki.tsuboi@monstar-lab.com>
2020-04-18 18:06:41 +0900

    Fixed seed_fu

--- db/fixtures/01_teacher.rb
+++ db/fixtures/01_teacher.rb
@@ -2,6 +2,6 @@
 
 Teacher.insert_all!(
   [
-    { id: 1, subject_id: 1 name: '山田太郎', created_at: Time.current, updated_at: Time.current },
+    { id: 1, subject_id: 1, name: '山田太郎', created_at: Time.current, updated_at: Time.current },
   ]
 )
